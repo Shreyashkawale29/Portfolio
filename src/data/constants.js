d9a7f8c0972817cd3ee0a0bdc64da1b4f4b57fd7
Shreyash kawale <kawalesn01@example.com>
2024-08-19 15:04:58 +0530

    update project discription

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/Shreyashkawale29/",
   resume:
-    "https://drive.google.com/file/d/1iNIFORfyOVMhU3da4k_z8AU8xoepftON/view",
+    "https://drive.google.com/file/d/1VqcECMgeMbmyDxNQVn0ZSRKQJuk-K57p/view",
   linkedin: "https://www.linkedin.com/in/shreyash-kawale-131345219/",
   twitter: "#",
   insta: "#",
